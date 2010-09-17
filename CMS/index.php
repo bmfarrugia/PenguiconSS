8b923570ab381a21c7945a407b4eb665649fc353
Jer Lance <github@ibloviate.org>
2010-09-17 19:25:16 -0400

    Adding CMS base files

---
+++ CMS/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+	
+<html>
+<head>
+	<meta HTTP-EQUIV= "Content-Type" CONTENT="text/html;charset=UTF-8">
+	<link REL="icon" HREF="favicon.ico">
+	<link REL="shortcut icon" HREF="favicon.ico">
+	<link REL="stylesheet" TYPE="text/css" HREF="default.css">
+		
+	<title>Penguicon</title>
+</head>
+
+<body>
+<div id="container">
+	<div id="header">
+		Here is the header. (header.php)
+		<?php //readfile("header.php"); ?>
+		<div id="menubar">
+			<ul id="menu">
+				<li><a href="#">This should </a></li>
+				<li><a href="#">be a series </a></li>
+				<li><a href="#">of menu </a></li>
+				<li><a href="#">items...</a></li>
+				<li><a href="#">Item 5</a></li>
+				<li><a href="#">(menubar.php)</a></li>
+			</ul>
+			<?php //readfile("menubar.php"); ?>
+		</div>
+	</div>
+	<div id="content">	
+		This is where we rock the content.
+
+<p>		Lorem ipsum dolor sit amet, sed mauris suscipit orci urna pellentesque, sit consequat, ullamcorper sed at ut, lacus tortor, mauris odio integer. Sociis dui sed, et vel gravida nullam natoque nisl wisi, libero torquent, lacinia in, blandit vulputate ut. Molestie velit eget, purus consequat, arcu neque molestie feugiat diam in, vitae sit a maecenas explicabo quisque metus, a luctus. Gravida libero pede integer, lacus libero, potenti varius class sit. Et sit mauris. Nascetur mi, amet fringilla quia suspendisse id morbi, duis bibendum, neque vestibulum risus rutrum diam pellentesque, pretium integer neque massa viverra adipiscing. Class unde eleifend quis, sit fringilla pellentesque fusce. Maecenas mattis per, pretium suscipit venenatis nec ut vestibulum, conubia nibh dolor ut elit sapien. Ipsum aut dolor. Id ante sint, aliquam in, ac curabitur metus ac suscipit dolor dolorem, fusce sed imperdiet. Vestibulum libero sit eros nam nisl, penatibus condimentum cum tempor delectus etiam, et libero, fusce eu wisi metus sodales ipsum pede, ut quisque massa. Duis risus nullam ac morbi phasellus, id mauris nulla velit vehicula viverra, mauris mollitia orci, morbi nonummy.</p>
+		<p>Mauris hac quam felis. Pellentesque et dui imperdiet suspendisse, semper sit dui tempor libero, arcu et vitae lectus. Etiam diam dui orci ornare. Donec arcu sodales ut lacinia in pede, convallis eleifend nullam quidem lectus erat in. Iaculis integer proident rutrum bibendum, id eu a metus eu. At diam maecenas nulla volutpat commodo, varius mattis, praesent pellentesque, integer per ligula, vestibulum amet. Nibh elementum. Ante quam nulla ut nec, aute duis eu orci enim augue. Eros tincidunt viverra maecenas gravida, eros hac, sem sed ultricies massa est. Aenean mollis, viverra arcu, turpis laoreet ipsum, ac scelerisque enim blandit sed, egestas venenatis urna. Sed leo accumsan vulputate at sapien ullamcorper, wisi aliquam, malesuada convallis.</p>
+	</div>	
+	<div id="footer">
+		And finally, a footer. Copyright info and all. (footer.php)
+		<?php //readfile("footer.php"); ?>
+	</div>
+</div>
+</body>
+</html>
